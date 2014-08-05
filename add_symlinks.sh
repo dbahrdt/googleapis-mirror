ed981caedaefd3c5404ea713904f836bd572e119
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-08-05 11:18:07 +0200

    update symlinks script

--- add_symlinks.sh
+++ add_symlinks.sh
@@ -2,29 +2,40 @@
 cd ajax/
 cd libs/
 cd jquery
-ln -s 1.4.0/ 1.4
 ln -s 1.3.0/ 1.3
-ln -s 1.8.0/ 1.8
-ln -s 1.7.0/ 1.7
-ln -s 1.6.0/ 1.6
+ln -s 1.4.0/ 1.4
 ln -s 1.5.0/ 1.5
+ln -s 1.6.0/ 1.6
+ln -s 1.7.0/ 1.7
+ln -s 1.8.0/ 1.8
+ln -s 1.9.0/ 1.9
+ln -s 2.0.0/ 2.0
+ln -s 2.1.0/ 2.1
 cd ..
 cd webfont/
 ln -s 1.0.0/ 1.0
+ln -s 1.1.0/ 1.1
+ln -s 1.2.0/ 1.2
+ln -s 1.3.0/ 1.3
+ln -s 1.4.0/ 1.4
+ln -s 1.5.0/ 1.5
 cd ..
 cd mootools/
 ln -s 1.3.0/ 1.3
 ln -s 1.4.0/ 1.4
+ln -s 1.5.0/ 1.5
 cd ..
 cd jqueryui/
 ln -s 1.6.0/ 1.6
 ln -s 1.7.0/ 1.7
 ln -s 1.8.0/ 1.8
 ln -s 1.9.0/ 1.9
+ln -s 1.10.0/ 1.10
 cd ..
 cd ext-core/
 ln -s 3.0.0/ 3.0
 ln -s 3.0.0/ 3
+ln -s 3.1.0/ 3.1
 cd ..
 cd dojo/
 ln -s 1.2.0/ 1.2
@@ -34,7 +45,7 @@ ln -s 1.5.0/ 1.5
 ln -s 1.6.0/ 1.6
 ln -s 1.7.0/ 1.7
 ln -s 1.8.0/ 1.8
-ln -s 1.5.0/ 1.5
+ln -s 1.9.0/ 1.9
 cd ..
 cd chrome-frame/
 ln -s 1.0.0/ 1.0
